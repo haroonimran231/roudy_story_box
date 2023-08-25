aed01a1fb6efad42693c04b5c3bf47ee7971e3e2
Haroon Imran <haroonimran231@gmail.com>
2023-08-25 17:38:42 +0500

    new

---
+++ lib/common/routes/index.dart
@@ -0,0 +1,13 @@
+library routes;
+
+export './routes.dart';
+export './names.dart';
+export 'package:flutter/material.dart';
+export 'package:get/get.dart';
+// export 'package:roudy_story_box/view/frames/onboarding1.dart';
+// export 'package:my_chat_app/view/register/index.dart';
+// export 'package:my_chat_app/view/connection/index.dart';
+// export 'package:my_chat_app/view/auth/index.dart';
+// export 'package:my_chat_app/view/user_info/index.dart';
+// export 'package:my_chat_app/view/home/index.dart';
+// export 'package:my_chat_app/view/messages/index.dart';
